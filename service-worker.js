81346b4dcf28a4a177356f7b8567de6f667e8ec5
tuscatalogosvip-byte <tuscatalogosvip@gmail.com>
2025-08-03 17:23:46 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,28 @@
-const CACHE_NAME = 'resize-images-cache-v1';
+const CACHE_NAME = 'pwa-example-cache-v1';
 const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
-  'https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js'
+  'icons/192.png',
+  'icons/512.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(resp => resp || fetch(event.request))
   );
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
     )
   );
 });
